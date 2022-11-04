fc344a09a2457cd1f2cb3f92c2569e129aaccfbf
azharanowar <azharanowar@gmail.com>
2022-11-04 10:00:52 +0600

    PHP foreach practie with Nested Array

--- day-06/app/classes/ArrayPractice.php
+++ day-06/app/classes/ArrayPractice.php
@@ -4,21 +4,49 @@ namespace App\classes;
 
 class ArrayPractice
 {
-    public $message, $names;
+    public $message, $names, $students;
     public function __construct() {
         $this->message = "Hello from Array Practice File";
         $this->names = ["Azhar Anowar", "Fozle Arafat", "Mohammad Ali"];
+
+        $this->students = [
+            0 => [
+                "name" => "Azhar Anowar",
+                "roll" => 181040,
+                "department" => "Computer",
+            ],
+            1 => [
+                "name" => "Fozle Arafat",
+                "roll" => 169756,
+                "department" => "Computer",
+            ],
+            2 => [
+                "name" => "Mohammad Ali",
+                "roll" => 169721,
+                "department" => "Computer",
+            ],
+        ];
     }
     public function index() {
         echo $this->message;
         echo "<br>";
 
 //        echo $this->names;
-
         print_r($this->names);
+
         echo "<br>";
         echo "<br>";
+
         var_dump($this->names);
+
+        echo "<br>";
+        echo "<br>";
+
+//        print_r($this->students);
+
+        foreach ($this->students as $student) {
+            echo $student["name"];
+        }
     }
 
 
