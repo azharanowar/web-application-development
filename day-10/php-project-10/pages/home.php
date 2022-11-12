b271ade150b1cd6fa6b53af6e5c806406a5c595c
azharanowar <azharanowar@gmail.com>
2022-11-12 10:41:24 +0600

    Bootstrap nav menu added

--- day-10/php-project-10/pages/home.php
+++ day-10/php-project-10/pages/home.php
@@ -6,10 +6,34 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home | Another Simple PHP Project</title>
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css"/>
 </head>
 <body>
-    <h1>This is heading!!!</h1>
+    <header class="bg-dark">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-dark py-3">
+                <a href="" class="navbar-brand text-uppercase fw-light text-muted"><span class="text-primary">Logo</span> Here</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainMenu">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="mainMenu">
+                    <ul class="navbar-nav mx-auto">
+                        <li class="nav-item"><a href="" class="nav-link">Home</a></li>
+                        <li class="nav-item"><a href="" class="nav-link">About</a></li>
+                        <li class="nav-item"><a href="" class="nav-link">Contact</a></li>
+                        <li class="nav-item dropdown">
+                            <a href="" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Services</a>
+                            <ul class="dropdown-menu">
+                                <li class=""><a href="" class="dropdown-item">Service 1</a></li>
+                                <li class=""><a href="" class="dropdown-item">Service 2</a></li>
+                                <li class=""><a href="" class="dropdown-item">Service 3</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </header>
 
 <script src="assets/js/bootstrap.bundle.js"></script>
 </body>
